4462e3de07d32c60309ebf57f53d7ba4f9b4a35b
insdout <49560124+insdout@users.noreply.github.com>
2022-03-30 18:03:26 +0300

    initial commit.

---
+++ Course_2/closest_pair.py
@@ -0,0 +1,30 @@
+# Given an adjacency matrix of a directed graph with weights of edges instead
+# of 0 and 1 (if there is no edge between the vertices that value is replaced
+# with float("inf")), find the pair of vertices with the minimum shortest path
+# among all pairs of vertices. If there are several such pairs, output any of them.
+#
+# If there is no such pair, output (-1, -1).
+
+from copy import deepcopy
+
+def closestPair(weight_matrix):
+    n = len(weight_matrix)
+    closest_pair = (-1, -1)
+    dist = deepcopy(weight_matrix)
+    for i in range(n):
+        dist[i][i] = 0
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    # YOUR CODE GOES HERE
+    print(dist)
+    return closest_pair
+
+if __name__ == "__main__":
+    weight_matrix = [[float('inf'), 5, 2],
+                     [5, float('inf'), 1],
+                     [2, 1, float('inf')]]
+    # check that your code works correctly on provided example
+    print(closestPair(weight_matrix))
+    assert closestPair(weight_matrix) == (1, 2), 'Wrong answer'
